95aa7601afa9fcf1df573605217ed25c7e066fcc
lunfee <1060970432@qq.com>
2022-07-16 14:30:23 +0800

    first commit

---
+++ src/main/java/xyz/lunfee/kafka_pdd/KafkaPddApplication.java
@@ -0,0 +1,13 @@
+package xyz.lunfee.kafka_pdd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaPddApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaPddApplication.class, args);
+    }
+
+}
---
+++ src/test/java/xyz/lunfee/kafka_pdd/KafkaPddApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.lunfee.kafka_pdd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaPddApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
